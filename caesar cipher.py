a535ad5120a03aeb4ba6766aab2acd5a9498f994
wynwastaken <delwyn.fayad@gmail.com>
2024-09-10 19:18:37 +0700

    new

---
+++ caesar cipher.py
@@ -0,0 +1,56 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+extra =[" "]
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def caesar(original_text,shift_amount,type_direction):
+    display_text = ""
+    for each_letter in original_text:
+        if each_letter in extra:
+            display_text += " "
+        
+
+        elif each_letter  not in alphabet:
+            display_text += "(X)"
+
+
+        elif type_direction == "encode":
+            index_value = alphabet.index(each_letter)
+            shifted_letter = (index_value + shift_amount)%len(alphabet)#jd gini dia bakal liat lebihny berapa
+            #misal z 25 kita forward 3 ,28 dibagi 6 sisa 2 itu indexny
+            display_text += alphabet[shifted_letter]
+
+
+        else:
+            index_value = alphabet.index(each_letter)
+            shifted_letter = (index_value - shift_amount)%len(alphabet)
+            display_text += alphabet[shifted_letter]
+
+
+
+
+
+    print(display_text)
+
+
+caesar(text,shift,direction)
